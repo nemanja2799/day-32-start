4bb82581565bb03f250ba0db4575a41a7af42a60
nemanja2799 <nemanjaivanovic2799@gmail.com>
2024-06-18 15:16:28 +0200

    Koriscenje modula date-time za dobijanje trenutnog vremena i izdvajanje podataka u posebne promenljive int tipa.

---
+++ main.py
@@ -0,0 +1,13 @@
+import datetime as dt
+
+now = dt.datetime.now()
+print(now)
+year = now.year
+month = now.month
+day = now.day
+hour = now.hour
+minute = now.minute
+second = now.second
+day_of_week = now.weekday()
+
+print(day_of_week)
